2b222a9f67214ebb92ce1703c218e4f0d0d06624
Zhiheng Yi <zyi093@gmail.com>
2022-12-26 18:20:57 +0800

    Add feed

--- scripts/feeds.js
+++ scripts/feeds.js
@@ -6,11 +6,11 @@
  */
 
 module.exports = [
-//   {
-//     name: 'open_app_filter',
-//     uri: 'https://github.com/destan19/OpenAppFilter.git',
-//     branch: 'oaf-5.1.0',
-//   },
+  {
+    name: 'open_app_filter',
+    uri: 'https://github.com/destan19/OpenAppFilter.git',
+    branch: 'oaf-5.1.0',
+  },
   {
     name: 'kenzok8_packages',
     uri: 'https://github.com/kenzok8/openwrt-packages.git',
@@ -26,19 +26,19 @@ module.exports = [
     uri: 'https://github.com/kenzok8/small-package.git',
     branch: 'main',
   },
-//   {
-//     name: 'kiddin9',
-//     uri: 'https://github.com/kiddin9/openwrt-packages.git',
-//     branch: 'master',
-//   },
-//   {
-//     name: 'jd_dailybonus',
-//     uri: 'https://github.com/jerrykuku/luci-app-jd-dailybonus',
-//     branch: 'master',
-//   },
-//   {
-//     name: 'lienol_packages',
-//     uri: 'https://github.com/Lienol/openwrt-package.git',
-//     branch: 'main',
-//   },
+  {
+    name: 'kiddin9',
+    uri: 'https://github.com/kiddin9/openwrt-packages.git',
+    branch: 'master',
+  },
+  {
+    name: 'jd_dailybonus',
+    uri: 'https://github.com/jerrykuku/luci-app-jd-dailybonus',
+    branch: 'master',
+  },
+  {
+    name: 'lienol_packages',
+    uri: 'https://github.com/Lienol/openwrt-package.git',
+    branch: 'main',
+  },
 ];
