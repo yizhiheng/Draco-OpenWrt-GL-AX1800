fc6d7a8fd7d7663b79e1d84e220a88796ff5b7c4
Zhiheng Yi <zyi093@gmail.com>
2022-12-26 23:12:45 +0800

    Add some packages

--- scripts/packages.js
+++ scripts/packages.js
@@ -5,50 +5,50 @@
  * @desc app 描述
  */
 module.exports = [
-  // {
-  //   name: 'luci-theme-argon',
-  //   desc: 'argon主题',
-  // },
-  // {
-  //   name: 'luci-app-argon-config',
-  //   desc: 'argon主题设置',
-  // },
+  {
+    name: 'luci-theme-argon',
+    desc: 'argon主题',
+  },
+  {
+    name: 'luci-app-argon-config',
+    desc: 'argon主题设置',
+  },
 //   {
 //     name: 'luci-app-store',
 //     desc: '应用商店',
 //   },
-  // {
-  //   name: 'luci-app-ramfree',
-  //   desc: '释放内存',
-  // },
-  // {
-  //   name: 'luci-app-wol',
-  //   desc: 'wol 网络唤醒',
-  // },
-  // {
-  //   name: 'luci-app-diskman',
-  //   desc: '磁盘管理工具',
-  // },
-  // {
-  //   name: 'luci-app-smartdns',
-  //   desc: 'smartdns DNS防污染',
-  // },
+  {
+    name: 'luci-app-ramfree',
+    desc: '释放内存',
+  },
+  {
+    name: 'luci-app-wol',
+    desc: 'wol 网络唤醒',
+  },
+  {
+    name: 'luci-app-diskman',
+    desc: '磁盘管理工具',
+  },
+  {
+    name: 'luci-app-smartdns',
+    desc: 'smartdns DNS防污染',
+  },
   // {
   //   name: 'luci-app-openclash',
   //   desc: 'clash的图形代理软件',
   // },
-  // {
-  //   name: 'luci-app-aria2',
-  //   desc: 'aria2下载工具',
-  // },
-  // {
-  //   name: 'luci-app-zerotier',
-  //   desc: 'ZeroTier内网穿透',
-  // },
-  // {
-  //   name: 'luci-app-turboacc',
-  //   desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
-  // },
+  {
+    name: 'luci-app-aria2',
+    desc: 'aria2下载工具',
+  },
+  {
+    name: 'luci-app-zerotier',
+    desc: 'ZeroTier内网穿透',
+  },
+  {
+    name: 'luci-app-turboacc',
+    desc: 'Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)',
+  },
   // {
   //   name: 'luci-app-xlnetacc',
   //   desc: '迅雷快鸟',
@@ -61,22 +61,22 @@ module.exports = [
     name: 'luci-app-cpufreq',
     desc: 'CPU 性能优化调节',
   },
-//   {
-//     name: 'luci-app-samba4',
-//     desc: 'samba4文件共享',
-//   },
-//   {
-//     name: 'luci-app-sqm',
-//     desc: '流量智能队列管理(QOS)',
-//   },
-//   {
-//     name: 'luci-app-advancedsetting',
-//     desc: '系统高级设置',
-//   },
-  // {
-  //   name: 'luci-app-ttyd',
-  //   desc: 'ttyd 终端',
-  // },
+  {
+    name: 'luci-app-samba4',
+    desc: 'samba4文件共享',
+  },
+  {
+    name: 'luci-app-sqm',
+    desc: '流量智能队列管理(QOS)',
+  },
+  {
+    name: 'luci-app-advancedsetting',
+    desc: '系统高级设置',
+  },
+  {
+    name: 'luci-app-ttyd',
+    desc: 'ttyd 终端',
+  },
 //   {
 //     name: 'luci-app-unblockneteasemusic',
 //     desc: '解锁网易云音乐 (无界面)',
@@ -85,10 +85,10 @@ module.exports = [
 //     name: 'luci-app-syncdial',
 //     desc: '多拨虚拟网卡（需要光猫支持多拨）',
 //   },
-  // {
-  //   name: 'luci-app-passwall',
-  //   desc: 'passwall代理软件',
-  // },
+  {
+    name: 'luci-app-passwall',
+    desc: 'passwall代理软件',
+  },
   {
     name: 'luci-app-ssr-plus',
     desc: 'ssr-plus 代理软件',
